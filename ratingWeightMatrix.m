fd3b1c088e44bb4bdc949a12010821aa7046d709
Faizan Shabbir <faizanshabbir@users.noreply.github.com>
2015-02-07 14:30:06 -0800

    Weight matrix using ratings
    Implemented weight matrix on rating values.  Factorization now occurring
    on matrix indicating whether user has watched movie or not.

---
+++ ratingWeightMatrix.m
@@ -0,0 +1,14 @@
+% In this case, our weight matrix is now using ratings.
+% Our R matrix will now use 0-1.  1 For where rating exists and 0 where it
+% does not.  Simply switch the weight and rating matrix from previous
+% problem.
+
+%Weight matrix
+W = (R==0);
+W =~ W;
+%Switch weight and r matrix
+newR = W;
+newW = R;
+
+k = [10, 50, 100];
+errArrProb2 = runModelAndError(newR,newW,k);
